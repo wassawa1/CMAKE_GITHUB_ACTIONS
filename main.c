065609b686469278a441e20b12b35bb8fe30ecfd
Wassawa <wwassa737@gmail.com>
2024-11-02 18:36:11 +0900

    revise cmake workflow

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
-#include<stdio.h>
-#include " mysource.h"
+#include <stdio.h>
+#include "mysource.h"
 
-int main(void){
-    
-    printf("Hello world\n");
+int main() {
+    int a, b;
+    char operation;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    printf("Enter operation (+, -, *, /): ");
+    scanf(" %c", &operation);
+
+    switch (operation) {
+        case '+':
+            printf("Result: %d\n", add(a, b));
+            break;
+        case '-':
+            printf("Result: %d\n", subtract(a, b));
+            break;
+        case '*':
+            printf("Result: %d\n", multiply(a, b));
+            break;
+        case '/':
+            if (b != 0)
+                printf("Result: %.2f\n", divide(a, b));
+            else
+                printf("Error: Division by zero is not allowed.\n");
+            break;
+        default:
+            printf("Invalid operation.\n");
+    }
+
     return 0;
-
 }
---
+++ mysource.h
@@ -0,0 +1,9 @@
+#ifndef MYSOURCE_H
+#define MYSOURCE_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+
+#endif
